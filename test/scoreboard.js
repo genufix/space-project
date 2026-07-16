adb5a2e22e85278b0266f01750d741c26f36673a
maximilian reinartz <Maximilian.j.reinartz@gmail.com>
2026-07-16 13:39:17 +0200

    point 1

--- test/scoreboard.js
+++
@@ -1,7 +0,0 @@
-const cursor = document.querySelector('.custom-cursor');
-
-document.addEventListener('mousemove', (e) => {
-  // Nutzt clientX und clientY für die exakte Position im Browserfenster
-  cursor.style.left = `${e.clientX}px`;
-  cursor.style.top = `${e.clientY}px`;
-});
